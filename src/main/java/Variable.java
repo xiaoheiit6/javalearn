95a4334c2441a0e462e226c3ba3fd2a32912de96
xiaoheiit6 <xiaoheiit@foxmail.com>
2022-06-24 20:49:25 +0800

    变量类型学习

--- src/main/java/Variable.java
+++ src/main/java/Variable.java
@@ -2,5 +2,8 @@
 public class Variable {
     public static void main(String[] args) {
         int a = 100;  //定义一个整型，int类型最常用，此外的还有short,long,byte等整型。
+        double b = 1.000;  //定义一个双精度浮点数。
+        var i = 12;  //从Java10开始，关键字var可以不需要指定变量类型来定义。
+        var d = 1.000;
     }
 }
