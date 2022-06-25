8fd57022614116e823e193fb409a329b38fc3a7e
xiaoheiit6 <xiaoheiit@foxmail.com>
2022-06-25 21:12:06 +0800

    运算符学习。

--- src/main/java/Operator.java
+++ src/main/java/Operator.java
@@ -4,9 +4,12 @@ public class Operator {
         System.out.println(100/2 + 50*10);  //在Java中，使用通常的运算符“+ - * /”都可以使用。值得注意的是，当参与运算的两个操作数都是整数时，“/”表示整数除法，否则，表示浮点数除法。
         System.out.println(15%2);  //整数的求余操作（取模操作）用“%”表示。
         //需要注意，整数被0除将产生一个异常，而浮点数被0除将会得到一个无穷大或NaN结果。
+
         //下面是数学函数与常量的学习。
         double x = 4;
         double y = Math.sqrt(x);  //计算一个数的平方根，可以使用sqrt方法。
         System.out.println(y);  //将输出x的平方根。
+        System.out.println(Math.PI  + Math.E);  //最后，提供了两个常量来表示Π和e的接近的近似值。
+        System.out.println(Math.sin(1));  //Math类也提供了一些常用的三角函数。以弧度制为单位。
     }
 }
