449c2ea4bca162efc33f90d37f1a3d4bfd66384b
xiaoheiit6 <xiaoheiit@foxmail.com>
2022-07-07 21:14:37 +0800

    运算符学习。

--- src/main/java/Operator.java
+++ src/main/java/Operator.java
@@ -14,5 +14,12 @@ public class Operator {
         System.out.println(y);  //将输出x的平方根。
         System.out.println(Math.PI  + Math.E);  //最后，提供了两个常量来表示Π和e的接近的近似值。
         System.out.println(Math.sin(1));  //Math类也提供了一些常用的三角函数。以弧度制为单位。
+
+        //数值类型之间的转换。
+        int n = 123456789;
+        float f = n;  //f是1.23456792E8 数量级是正确的，但是会损失一些精度。
+        double u = 9.997;
+        int ux = (int) u;  //强制类型转换，语法格式是在圆括号中指定想要转换的目标类型，后面紧跟要强制转换的变量名。此时的u变量转换为9，强制类型转换通过截断小数部分将浮点值转换为整型。
+        int uux = (int) Math.round(u);  //使用四舍五入的方法来转换为一个最接近的整数。可以使用Math.round的方法。调用round，仍需要使用强制类型转换。原因是round返回类型是long类型。
     }
 }
